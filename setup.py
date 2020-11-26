0a05d17e2d573e013dd4d8ad40d3580918619c11
smith.fu <smith.fu@ubtrobot.com>
2020-11-26 18:01:49 +0800

    init: project created

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='local_useragent',
+    version='0.0.1',
+    packages=['local_useragent'],
+    url='',
+    license='',
+    author='smith',
+    author_email='fxfpro@163.com',
+    description='fake_useragent use local data',
+
+    install_requires=[
+        'fake_useragent'
+    ],
+)
